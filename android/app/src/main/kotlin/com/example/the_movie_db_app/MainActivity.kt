5a8bd2a63e2020442831d81574523f8e44dd26b5
Ujjwal <ujjwal.dey55@gmail.com>
2025-08-28 02:12:40 +0530

    inital flutter set up

---
+++ android/app/src/main/kotlin/com/example/the_movie_db_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.the_movie_db_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'features/movie/movie.dart';
+
+void main() {
+  runApp( Material(
+    child: Scaffold(
+      body: Text('hello world'),
+    )
+  ));
+}
+
